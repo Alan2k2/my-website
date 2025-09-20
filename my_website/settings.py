c3b0c89d87e4e3235682b3045b4fee29cb8697ce
Alan Shaju <140711697+Alan2k2@users.noreply.github.com>
2025-09-20 15:34:41 +0530

    Update settings.py

--- my_website/settings.py
+++ my_website/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = 'django-insecure-fmmz2)n8y(=n+r)-0!0yval5brrz2&$pft&!3%ypy(qza3+ugc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["djangoproject-mysite.onrender.com"]
+ALLOWED_HOSTS = [
+    "djangoproject-mysite.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
+
 
 
 # Application definition
